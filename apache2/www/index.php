468c59a7b86e56abc88263b2813f806705381b50
pichai chin <pichai.chin@gmail.com>
2016-11-16 15:46:36 +0700

    update docker compose

---
+++ apache2/www/index.php
@@ -0,0 +1,8 @@
+<div>
+	<b>apache is work...</b>
+	<?php
+		echo"<p> <b>Environment</b> : ",$_SERVER['CI_ENV'],"</p>";
+	?>
+	<img src="//farms.abc.com/nginx-logo.png" alt="nginx-subdomain" />
+	<img src="farms/nginx-logo.png" alt="nginx-path" />
+</div>
