17a11f7b108644577222dcba9a7cb34eb438e10f
pichai chin <pichai.chin@gmail.com>
2016-11-17 21:04:37 +0700

    update complete repo

--- apache2/www/ptest.php
+++ apache2/www/ptest.php
@@ -1,8 +1,15 @@
 <div>
+	<p>
 	<b>apache is work...</b>
 	<?php
 		echo"<p> <b>Environment</b> : ",$_SERVER['CI_ENV'],"</p>";
 	?>
 	<img src="//farms.abc.com/nginx-logo.png" alt="nginx-subdomain" />
 	<img src="farms/nginx-logo.png" alt="nginx-path" />
+	</p>
+
+
+	<?php
+		phpinfo();
+	?>
 </div>
